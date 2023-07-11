a889693e6b671cb2b61b56aee86718797d3874d8
Umar Farooq <umar.wzbd@gmail.com>
2023-07-11 10:46:40 +0500

    Adding First Template

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def starting_page(request):
-    return HttpResponse("Starring Page!")
+    return render(request, "blog/index.html")
 
 def posts(request):
     return HttpResponse("Posts Page!")
