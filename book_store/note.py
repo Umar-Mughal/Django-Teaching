bf00debfcfbc0016aed5305fb81cce134a08f42f
Umar Farooq <umar.wzbd@gmail.com>
2023-07-15 19:43:10 +0500

    Inserting Data < Model.save() >

--- book_store/note.py
+++ book_store/note.py
@@ -1,1 +1,7 @@
 # A custom note file just to record stuff executed on python interactive shell!
+
+# --- INSERTION --- #
+from book_outlet.models import Book
+
+harry_potter = Book(title="Harry Potter", rating=5, author="dont know", is_bestselling=True)
+harry_potter.save()  # Here actual DB query is executed
