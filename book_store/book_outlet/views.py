a7044f251fe2dde72a1725d33c5281a4198c4538
Umar Farooq <umar.wzbd@gmail.com>
2023-07-20 13:35:05 +0500

    Orderging

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -5,7 +5,7 @@ from django.db.models import Avg
 # Create your views here.
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("-rating")
     num_books = books.count()
     avg_rating = books.aggregate(Avg("rating"))
     return render(request, "book_outlet/index.html", {
