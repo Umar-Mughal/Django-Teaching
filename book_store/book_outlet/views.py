d2ce1049a0823b612237cf555077c4525f4c84f6
Umar Farooq <umar.wzbd@gmail.com>
2023-07-20 12:44:48 +0500

    Rendering Single Book Data in Template

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
+from django.http import Http404, HttpResponse
 # Create your views here.
 
 def index(request):
     books = Book.objects.all()
     return render(request, "book_outlet/index.html", {
         "books": books
+    })
+
+
+def book_detail(request, id):
+    # try:
+    #     book = Book.objects.get(id=id) # or can be pk=id instead of id=id
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(Book, pk=id)
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling
     })
