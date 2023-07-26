cd411adab07f40f262c0f90dbbeab955b22fa4a1
Umar Farooq <umar.wzbd@gmail.com>
2023-07-26 13:44:48 +0500

    Cross Model Queries | Getting Book Data through Author Model
    Description:
    
    Django automatically adds modelname_set (ex: book_set) property to query related data. Although you can define it yourself in models.py on foreign key field.

--- book_store/python_shell_notes/cross_model_quries.py
+++ book_store/python_shell_notes/cross_model_quries.py
@@ -8,3 +8,11 @@ books_by_rowling = Book.objects.filter(author__last_name__contains="wling")
 books_by_rowling
 >>> <QuerySet [<Book: Harry Potter 1 (Rating:5)>]>
 
+# INVERSE
+# Getting All Author's Books -
+# here modelName__set (ex: book_set) is auto defined field  if we do not set ourselves.
+jkr = Author.objects.get(first_name="J.K")
+jkr
+>>> <Author: Author object (1)>
+jkr.book_set.all()
+<QuerySet [<Book: Harry Potter 1 (Rating:5)>]>
