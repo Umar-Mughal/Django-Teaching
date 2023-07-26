c9bbb750154c72088361bae988f44a6a330f618a
Umar Farooq <umar.wzbd@gmail.com>
2023-07-26 15:14:27 +0500

    Managing onetoone in Admin
    Description:
    
    1- Setup configurations in code
    2- Attach address while adding or updating author in Admin

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -9,6 +9,9 @@ class Address(models.Model):
     postal_code = models.CharField(max_length=5)
     city = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.stree}, {self.postal_code}, {self.city}"
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
