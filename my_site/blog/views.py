511bef002e8d59a505933f60d9a31edb42978f4b
Umar Farooq <umar.wzbd@gmail.com>
2023-07-26 17:31:31 +0500

    Fetching Posts for Starting Page

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,6 +1,7 @@
 from datetime import date
 
 from django.shortcuts import render
+from .models import Post
 
 all_posts = [
     {
@@ -74,8 +75,7 @@ def get_date(post):
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3] # It will directly just fetch 3 records from DB, not just fetch all and then slice first 3.
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
