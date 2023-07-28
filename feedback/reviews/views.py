d1126b1287d334b043ee9999d89c1c425c2f7b5f
Umar Farooq <umar.wzbd@gmail.com>
2023-07-28 21:56:57 +0500

    FormView | Replacing get Method

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -3,22 +3,15 @@ from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 from .forms import ReviewForm
 from .models import Review
 # Create your views here.
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()  # New Empty Form
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
-
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/thank-you')
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    
 
 class ThankYouView(TemplateView):
     template_name = "reviews/thank_you.html"
