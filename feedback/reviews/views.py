cfaf9a1a5ff1333a9f03981a93fac579733332a1
Umar Farooq <umar.wzbd@gmail.com>
2023-07-28 18:35:43 +0500

    Updating Data to DB with ModelForm

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
-
+from .models import Review
 # Create your views here.
 
 def review(request):
     if request.method == 'POST':
-        form = ReviewForm(request.POST)
+        existing_data = Review.objects.get(pk=1)
+        form = ReviewForm(request.POST, instance=existing_data)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/thank-you')
