5e78373873961b7fbb5d71f349548a9622c31c33
Umar Farooq <umar.wzbd@gmail.com>
2023-07-28 18:28:03 +0500

    Configuring the ModelForm

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -4,6 +4,15 @@ from .models import Review
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        # fields = ['user_name', 'review_text'] # selected fields
         fields = '__all__' # all fields will be added in form
-        # exclude = ['owner_comment'] # the fields which you do not want to render in form
+        labels = {
+            "user_name": "Your Name",
+            "review_text": "Your Feedback",
+            "rating": "Your Rating"
+        }
+        error_messages = {
+            "user_name": {
+                "required": "Your name must not be empty!",
+                "max_length": "Please enter a shorter name!"
+            }
+        }
