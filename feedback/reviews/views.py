1acf4723ee84bd0cda31acb4c95c3d08bfe65dd8
Umar Farooq <umar.wzbd@gmail.com>
2023-07-28 20:12:56 +0500

    TemplateView | A Replacement to Get with Normal View

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
+from django.views.generic.base import TemplateView
 from .forms import ReviewForm
 from .models import Review
 # Create your views here.
@@ -18,8 +19,12 @@ class ReviewView(View):
             form.save()
             return HttpResponseRedirect('/thank-you')
 
-class ThankYouView(View):
-    def get(self, request):
-        return render(request, "reviews/thank_you.html")
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = "This Works!"
+        return context
 
 
