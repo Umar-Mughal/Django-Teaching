eb259c35d647f5427662ce412a51ffe3169ba9cf
Umar Farooq <umar.wzbd@gmail.com>
2023-07-31 18:51:40 +0500

    Converting Views to Class Based Views
    Description:
    
    - Arguably you can say class based views might have more code than functions based approach, you can stick to either approach. Here we're just practicing class based views we learned before
    
    - for DetailView make sure in url we have slug field validation, in order to search the post by slug automatically

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,7 +1,39 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+
 from .models import Post
 
 # Create your views here.
+
+# Starting or Landing Page
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ['-date']
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3] # It's equal to Post.objects.all()[:3]
+        return data
+
+# All Posts Page
+class AllPostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ['-date']
+    context_object_name = "all_posts"
+
+# Single Post
+class SinglePostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+
+    # fetching tags data as well
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_tags'] = self.object.tags.all()
+        return context
 
 def starting_page(request):
     latest_posts = Post.objects.all().order_by("-date")[:3] # It will directly just fetch 3 records from DB, not just fetch all and then slice first 3.
