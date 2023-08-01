31db045459e70e151025fc2889fa922698d86c6c
Umar Farooq <umar.wzbd@gmail.com>
2023-08-01 14:29:07 +0500

    Handling Comment Form Submission

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Post
 from .forms import CommentForm
@@ -26,16 +29,35 @@ class AllPostsView(ListView):
     context_object_name = "all_posts"
 
 # Single Post
-class SinglePostView(DetailView):
+class SinglePostView(View):
     template_name = "blog/post-detail.html"
     model = Post
 
-    # fetching tags data as well
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['post_tags'] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False) # commit=False means will not save yet
+            comment.post = post # attaching post reference here
+            comment.save() # manually calling save now
+            
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form # if the form is invalid errors will be sent
+        }
+        return render(request, "blog/post-detail.html", context)
 
 def starting_page(request):
     latest_posts = Post.objects.all().order_by("-date")[:3] # It will directly just fetch 3 records from DB, not just fetch all and then slice first 3.
