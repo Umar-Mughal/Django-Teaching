13069fede44d7bf7d6cfb0568eac1e695a02585e
Umar Farooq <umar.wzbd@gmail.com>
2023-09-02 11:55:07 +0500

    Forms: Sending Email in Views | Rendering Form in Templates

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
 from .forms import EmailPostForm
 from .models import Post
 
@@ -35,11 +36,18 @@ def post_detail(request, year, month, day, slug):
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            # ... send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url} \n\n {cd['name']}\'s comments: {cd['comments']}"
+
+            send_mail(subject, message, 'umar.async@gmail.com', [cd['to']])
+            sent = True
+
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
